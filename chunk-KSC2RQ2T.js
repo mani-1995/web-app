f4fea35ae17e419afec501ba20133e9b8541a9d3
mani-1995 <159513761+mani-1995@users.noreply.github.com>
2026-07-15 09:42:52 +0000

    Deploying to gh-pages from @ mani-1995/web-app@4493297444c5d27e8f769668daea3c18bea82c00 🚀

---
+++ chunk-KSC2RQ2T.js
@@ -0,0 +1,1 @@
+import{Ha as i}from"./chunk-MXSRGXWA.js";import{ea as e,ja as r}from"./chunk-Y5HIBKVH.js";var m=(()=>{class t{constructor(){this.systemService=r(i)}resolve(){return this.systemService.getConfigurations()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{m as a};
